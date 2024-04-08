c2159738e57a2e56a87cb5bb6fe722d417f5e0fb
Chandni Ansari <74239667+cansari04@users.noreply.github.com>
2024-04-08 18:04:33 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
@@ -13,9 +14,9 @@ y = np.array([np.where(labels == x) for x in y]).flatten()
 # scaler = StandardScaler()
 # X_scaled = scaler.fit_transform(X)
 
-# model = LogisticRegression(max_iter=1000, random_state=42).fit(X_scaled, y)
+#model = LogisticRegression().fit(X, y)
+#model = LogisticRegression(max_iter=1000, random_state=42).fit(X_scaled, y)
 model = GaussianNB().fit(X, y)
-
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
